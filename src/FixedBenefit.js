54a441d2413c8a3b5b66f2f2f50a9224914716a2
isrg <isrg@isrg.com>
2020-06-13 12:17:32 +0900

    first commit

---
+++ src/FixedBenefit.js
@@ -0,0 +1,13 @@
+{/* <input type="file" id="seifile"></input>
+
+<script>
+    
+    var obj1 = document.getElementById("seifile");
+
+    obj1.addEventListener("change", function (evt) {
+        var file = evt.target.files;
+        alert(file[0].name + "を取得しました。");
+        
+    }, false);
+
+</script> */}
